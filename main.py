3ed2206418a341f3c3b118f9cdca33de401d2c8f
Manidakis Marinos <m.manidakis@cloudisimo.gr>
2019-02-05 19:01:41 +0200

    remove more funcitons keep only the class and the file reading part

--- main.py
+++ main.py
@@ -25,30 +25,6 @@ def readFile(filename):
         reader = csv.reader(log, delimiter=',', quotechar='"')
         return [l for l in reader]
 
-# sum all resouce quantities by account and type of resource
-def sumByAccount(log, resourceTypes):
-    total = defaultdict(list)
-
-    for entry in log:
-        if not total[entry.accountName]:
-            total[entry.accountName].extend([0] * len(resourceTypes))
-
-        total[entry.accountName][resourceTypes.index(entry.resource)] += int(entry.resourceQuantity)
-
-    return total
-
-# print into a nice format the data
-def printLeaderboard(totals, resourceTypes, outputfile):
-    table = PrettyTable(['Account', *resourceTypes])
-    
-    for account in totals:
-        table.add_row([account, *totals[account]])
-
-    table_txt = table.get_string()
-    with open(outputfile, 'w') as file:
-        file.write(table_txt)
-
-
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Neverwinter donations parser')
@@ -60,6 +36,7 @@ if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('./config')
     path = config.get('OPTIONS', 'LogLocation')
+    resourceTypes = json.loads(config.get('OPTIONS', 'ResourceTypes'))
 
     # get all the logfiles with .csv extension
     logfiles = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f.endswith(".csv")]
@@ -75,7 +52,6 @@ if __name__ == '__main__':
         tempLog.extend(readFile(path + os.sep + f))
         log = [list(i) for i in set(tuple(i) for i in tempLog)]
 
-    resourceTypes = json.loads(config.get('OPTIONS', 'ResourceTypes'))
 
     # convert to a list of objects
     log = [LogEntry(*tuple(e)) for e in log]
@@ -83,5 +59,19 @@ if __name__ == '__main__':
     # keep only the data for the resource types specified in the config
     log = [e for e in log if e.resource in resourceTypes]
     
-    log = sumByAccount(log, resourceTypes)
-    printLeaderboard(log, resourceTypes, outputfile)
+    # Sum resource quantities for each account
+    data = defaultdict(list)
+    for entry in log:
+        if not data[entry.accountName]:
+            data[entry.accountName].extend([0] * len(resourceTypes))
+
+        data[entry.accountName][resourceTypes.index(entry.resource)] += int(entry.resourceQuantity)
+
+    # export into a pretty table the data
+    table = PrettyTable(['Account', *resourceTypes])
+    for account in data:
+        table.add_row([account, *data[account]])
+
+    table_txt = table.get_string()
+    with open(outputfile, 'w') as file:
+        file.write(table_txt)
